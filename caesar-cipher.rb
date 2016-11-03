d9a16b224b22eba0f275c0b39cf7eaff6bf8c249
“JustinVx” <justin.venbrux@gmail.com>
2016-11-03 18:23:48 +0100

    Solved the Caesar Cipher.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -4,23 +4,20 @@ def ceasar_ciper(input_s, change)
   alphabet = 'abcdefghijklmnopqrstuvwxyz'
   output = ""
   for i in 0...input_s.size do
-    up = false
-    up = true if input_s[i] =~ /[A-Z]/
+    /[A-Z]/ =~ input_s[i] ? (up = true) : (up = false)
+    output += input_s[i] if !alphabet.include?(input_s[i].downcase)
     for j in 0...alphabet.size do
       if alphabet[j] == input_s[i].downcase
         if j+change < alphabet.size
-          #input_s[i] = alphabet[j+change]
-          k = j + change
-          print "#{alphabet[k]}"
-          input_s[i] = alphabet[j]
+          up ? (output += alphabet[j+change].upcase) : (output += alphabet[j+change])
         else
-          input_s[i] = alphabet[j+change-alphabet.size]
+          up ? (output += alphabet[j+change-alphabet.size]) : (output += alphabet[j+change-alphabet.size])
         end
       end
     end
     input_s[i] = input_s[i].upcase if up
   end
-  puts input_s
+  return output
 end
 
-ceasar_ciper("Krijg toch allemaal de volkolere xx bert", 3)
+puts ceasar_ciper("Krijg toch allemaal de volkolere xx Bert", 3)
